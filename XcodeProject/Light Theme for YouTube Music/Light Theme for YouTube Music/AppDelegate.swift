d919433e7e373877402d4860867c2813bcea35b8
Murad Jabir <muradjabir@Murads-MacBook-Air.local>
2026-09-17 14:16:19 +0530

    Build: Add Xcode project files and gitignore

---
+++ XcodeProject/Light Theme for YouTube Music/Light Theme for YouTube Music/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Light Theme for YouTube Music
+//
+//  Created by Murad Jabir on 2026-09-17.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
